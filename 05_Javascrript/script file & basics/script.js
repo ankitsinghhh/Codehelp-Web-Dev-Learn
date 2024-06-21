5a1e31c04041b43d61f01bdd6b71c77559d5fe35
ankitsinghhh <ankitsingh79834@gmail.com>
2024-06-22 00:51:54 +0530

    class 1 js completed

---
+++ 05_Javascrript/script file & basics/script.js
@@ -0,0 +1,62 @@
+console.log("hello")
+
+let a=5; 
+console.log(a);
+
+// let b=6;
+
+// console.log(b)
+
+// let b = 10:
+
+// console.log(b);
+
+// redeclaration is not possible in case of let
+
+// let age=75
+
+// if(age<=25){
+//     console.log("you are young")
+// }
+// else if(age>25 && age<40)
+//     {
+//         console.log("you are middle aged and can go gym")
+//     }
+// else if(age>40 && age<65){
+//     console.log("you shoudl retire anytime soon")
+// }
+// else{
+//     console.log("you are old and can die anytime soon")
+// }
+
+// let choice = 0;
+
+// switch(choice){
+//     case 1: console.log("you chose 1")
+//     break;
+//     case 2: console.log("you chose 2")
+//     break;
+//     case 3: console.log("you chose 3")
+//     break;
+//     default: console.log("invalid choice")
+// }
+
+// for(let i =0 ; i<10;i++){
+//     console.log(i)
+// }
+
+// let i =0;
+
+// while(i<10){
+//     console.log(i)
+//     i++;
+// }
+
+
+let i = 0 ;
+
+do{
+    
+    console.log(i)
+    i++
+}while(i<10)
