a52c2c5f08e80b24dd8d9229d5bbd03f2b94c617
ankitsinghhh <ankitsingh79834@gmail.com>
2024-06-24 08:50:14 +0530

    counter app created by myself using addeventlistener and textContent

---
+++ 06_Javascript_Advanced/unwrap classs/CounterApp by myself/script.js
@@ -0,0 +1,37 @@
+//using addEventListener and .textContent
+
+let container = document.querySelector('.number');
+
+let counterString = container.innerHTML;
+let counter = Number(counterString);
+
+//checks while development
+// console.log(counter)
+// console.log(typeof counter)
+
+counter=1000;
+
+
+
+// to modify the counter
+container.textContent = counter;
+
+
+
+let decbtn= document.querySelector('.dec-btn');
+let incbtn = document.querySelector('.inc-btn')
+
+decbtn.addEventListener('click',function(){
+    counter--;
+    container.textContent = counter;
+})
+
+incbtn.addEventListener('click',function(){
+    counter++;
+    container.textContent = counter;
+})
+
+
+
+
+
