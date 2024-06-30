7eb9ae427dbd18b81b6c1af8bc251cb4b9a00066
ankitsinghhh <ankitsingh79834@gmail.com>
2024-06-30 23:52:15 +0530

    class 6 testimonial project done

---
+++ 07_React/1_Basics/Class-6/testimonial_slider_project/src/components/testimonials.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Card from "./Card"
+import {useState} from 'react'
+
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+const Testimonials = (props) => {
+    let reviews = props.reviews
+    const [index, setIndex] = useState(0)
+
+    function leftShiftHandler(){
+        if(index-1<0){
+            setIndex(reviews.length-1)
+        }
+        else{
+            setIndex(index-1)
+        }
+    }
+
+    function rightShiftHandler(){
+        if(index + 1>= reviews.length){
+            setIndex(0)
+        }
+        else{
+            setIndex(index+1)
+        }
+
+    }
+
+    function surpriseHandler(){
+        let randomIndex = Math.floor(Math.random() * reviews.length)
+        setIndex(randomIndex)
+    }
+
+    return (
+        <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center m-10 p-10 transition-all duration-200 hover:shadow-xl rounded-md'>
+            <Card review={reviews[index]}></Card>
+
+            <div className='flex text-3xl gap-3 mt-9 text-violet-400 font-bol mx-auto justify-center'>
+                <button 
+                onClick={leftShiftHandler}
+                className='cursor-pointer hover:text-violet-500 '>
+                    <FiChevronLeft />
+                </button>
+                <button 
+                onClick={rightShiftHandler}
+                className='cursor-pointer hover:text-violet-500'>
+                    <FiChevronRight />
+                </button>
+            </div>
+
+            <div className='mt-6'>
+                <button
+                onClick={surpriseHandler}
+                className='bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-xl text-white'>
+                    Surprise Me
+                </button>
+            </div>
+
+        
+
+    </div >
+  )
+}
+
+export default Testimonials
