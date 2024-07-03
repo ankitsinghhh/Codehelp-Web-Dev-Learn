4b26fe6113ba6bca3c99ef7b69979abdd33f08ee
ankitsinghhh <ankitsingh79834@gmail.com>
2024-07-03 11:46:57 +0530

    starting react advanced lecture 2 blog website

---
+++ 07_React/3_Advanced/Class_2_3/Blog_Website/src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+// {/* learnign context API  
+
+// Props drilling -> sending varibales &  function to child from parent , from that child to that child's children and so on 
+
+// State Lifting -> sending varibales & function to parent from child to that parent's parent and so on
+
+// Context API -> passing variables directly between components without passing them as props
+
+// context - > snapshot of data
+
+// ek baar data create krdo context api se , fir kahi v access kr skte h uske children me 
+
+// */}
+
+// Rules to use Context API , 
+
+//1. create context 
+// jis compoent ko denge , wo aur uske saare children access kr skte h is context ko 
+
+//2. providing context / provision of context
+
+//3. consuming context
